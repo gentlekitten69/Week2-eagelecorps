afbb163fd2de35bacf570ac62c449efbb5ea2b5d
gentlekitten69 <tiahalibel2010@hotmail.com>
2023-09-13 18:58:08 -0400

    trying to copy the files to my laptop

---
+++ wordle.js
@@ -0,0 +1,12 @@
+let wordChar = document.getElementById();
+let wordArr = ["about", "chair", "apple", "grape", "pizza"]
+
+
+
+function sumbitGuess() {
+    userGuess;
+    randomWord;
+    let userGuess = document.getElementById("userGuess").value
+    let (randomWord = Math.floor(Math.random() * wordArr.length));
+   
+} 
